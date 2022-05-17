858c5454eba4720606eb5336a2332035fd8acad0
sbudi4 <83524990+sbudi4@users.noreply.github.com>
2022-05-17 19:11:49 +1000

    Uploading VSCODE work

---
+++ src/VolcanoData.js
@@ -0,0 +1,15 @@
+import FetchVolcanoData from "./API/FetchVolcanoData";
+import NavBarHeader from "./components/NavBarHeader";
+import "./styles.css";
+
+export default function VolcanoData() {
+  return (
+    <div>
+      <NavBarHeader />
+      <div className="container">
+        <h1>Volcano Data</h1>
+      </div>
+      <FetchVolcanoData />
+    </div>
+  );
+}
