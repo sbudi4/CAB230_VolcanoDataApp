b518ee54d169e74de527d6958b4098ee2436ab36
Selwyn <nywlesnawaidub@gmail.com>
2022-05-19 17:13:43 +1000

    pooper

--- src/VolcanoData.js
+++ src/VolcanoData.js
@@ -1,6 +1,8 @@
 import FetchVolcanoData from "./components/API/FetchVolcanoData";
+import CountrySelection from "./components/CountrySelection";
 import DataTable from "./components/DataTable";
 import NavBarHeader from "./components/NavBarHeader";
+
 import "./styles.css";
 
 export default function VolcanoData() {
@@ -11,7 +13,9 @@ export default function VolcanoData() {
         <h1>Volcano Data</h1>
       </div>
       <FetchVolcanoData />
-      <DataTable />
+      <CountrySelection />
+
+      <div className="DataTable">      <DataTable /></div>
 
     </div>
   );
