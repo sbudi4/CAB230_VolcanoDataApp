cb2f481fc4a3a933b6cf7244728ace23156e151e
Selwyn <nywlesnawaidub@gmail.com>
2022-05-19 16:10:20 +1000

    poop

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,37 +1,71 @@
-// This file contains the Ag-Grid Table that displays Volcano Data.
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import { useNavigate } from "react-router-dom";
 
-// Importing dependencies...
-import React, {useState} from 'react';
-import {AgGridColumn, AgGridReact} from 'ag-grid-react';
-
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+// const table = {
+//   columns: [
+//     { headerName: "Make", field: "make" },
+//     { headerName: "Model", field: "model" },
+//     {
+//       headerName: "Price",
+//       field: "price",
+//       sortable: true,
+//       filter: "agNumberColumnFilter"
+//     }
+//   ],
+//   rowData: [
+//     { make: "Toyota", model: "Camry", price: 28000 },
+//     { make: "Ford", model: "Focus", price: 16700 },
+//     { make: "Hyundai", model: "Kona", price: 23500 }
+//   ]
+// };
 
 export default function DataTable() {
+  const [rowData, setRowData] = useState([]);
+  const navigate = useNavigate();
 
-    const [rowData, setRowData] = useState();
+  const columns = [
+    { headerName: "Title", field: "title", sortable: true, filter: true },
+    { headerName: "Author", field: "author", sortable: true },
+    { headerName: "Edition Count", field: "editionCount", sortable: true },
+    { headerName: "Book ID", field: "id", sortable: true }
+  ];
 
-    const [colDefs, setColDefs] = useState([
-        {field: 'make'},
-        {field: 'model'},
-        {field: 'price', editable: 'true'},
-    ]);
+  useEffect(() => {
+    fetch("https://openlibrary.org/subjects/drama.json?published_in=2000")
+      .then((res) => res.json())
+      .then((data) => data.works)
+      .then((works) =>
+        works.map((book) => {
+          return {
+            title: book.title,
+            author: book.authors[0].name,
+            editionCount: book.edition_count,
+            id: book.cover_id
+          };
+        })
+      )
+      .then((books) => setRowData(books));
+  }, []);
 
-    React.useEffect(() => {
-        fetch('https://www.ag-grid.com/example-assets/row-data.json')
-            .then(result => result.json())
-            .then(rowData => setRowData(rowData))
-    }, []);
-    
-   return (
-       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>   
-           <AgGridReact
-                defaultColDef={{sortable: true, filter: true }}
-                pagination={true}
-                rowData={rowData}
-                columnDefs={colDefs}
-                >
-           </AgGridReact>
-       </div>
-   )
-};
+  return (
+
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "300px",
+          width: "800px"
+        }}
+      >
+        <AgGridReact
+          columnDefs={columns}
+          rowData={rowData}
+          pagination={true}
+          paginationPageSize={7}
+          onRowClicked={(row) => navigate(`/book?title=${row.data.title}`)}
+        />
+      </div>
+  );
+}
